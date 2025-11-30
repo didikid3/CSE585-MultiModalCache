6adfeb4c5b8566e6eb68e8a7bb7fcadc63f4a9c2
Brandon Chao <34197234+didikid3@users.noreply.github.com>
2025-11-30 16:29:07 -0500

    Batch script

---
+++ SLURM_exe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=image-net
+#SBATCH --account=eecs585f25_class
+#SBATCH --partition=spgpu
+#SBATCH --gpus=1
+#SBATCH --time=04:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=40g
+#SBATCH --mail-user=bchao@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=fine-tune.out
+
+python image_net_eval.py
